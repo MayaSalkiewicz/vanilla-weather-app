9b1354e99e793e968cc6bfa69946027faa5bc9be
MayaSalkiewicz <94534283+MayaSalkiewicz@users.noreply.github.com>
2022-01-24 11:07:57 +0100

    added a current Date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
-let apiKey = "3c26ee9ecca3230f1645ab722354817a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function showMeWeather(response) {
   let cityElement = document.querySelector("#city-name");
@@ -8,6 +30,7 @@ function showMeWeather(response) {
   let pressureElement = document.querySelector("#pressure");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
 
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -15,6 +38,11 @@ function showMeWeather(response) {
   pressureElement.innerHTML = response.data.main.pressure;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
 }
+
+let apiKey = "3c26ee9ecca3230f1645ab722354817a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showMeWeather);
